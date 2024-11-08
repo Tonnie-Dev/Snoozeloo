e890d1055a10f26622707cc50d38635d91929414
Tonnie <vontonnie@gmail.com>
2024-11-08 16:34:36 +0300

    Add AlarmReceiver Class

--- app/src/main/java/com/tonyxlab/data/repository/AlarmSchedulerImpl.kt
+++ app/src/main/java/com/tonyxlab/data/repository/AlarmSchedulerImpl.kt
@@ -1,11 +1,16 @@
 package com.tonyxlab.data.repository
 
+import android.app.AlarmManager
+import android.content.Context
+import com.tonyxlab.common.utils.toEpochMillis
 import com.tonyxlab.domain.model.AlarmItem
 import com.tonyxlab.domain.repository.AlarmScheduler
 
-class AlarmSchedulerImpl: AlarmScheduler {
+class AlarmSchedulerImpl(context:Context): AlarmScheduler {
+
+   private val alarmManager = context.getSystemService(AlarmManager::class.java)
     override fun schedule(item: AlarmItem) {
-        TODO("Not yet implemented")
+        alarmManager.setAndAllowWhileIdle( AlarmManager.RTC_WAKEUP, item.toEpochMillis())
     }
 
     override fun cancel(item: AlarmItem) {
