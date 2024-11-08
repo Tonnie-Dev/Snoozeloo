75788970db5ab36a574aaf304cdf8c58686fc0ae
Tonnie <vontonnie@gmail.com>
2024-11-08 17:40:16 +0300

    Clean Up MainActivity

--- app/src/main/java/com/tonyxlab/MainActivity.kt
+++ app/src/main/java/com/tonyxlab/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.tonyxlab.ui.theme.SnoozelooTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             SnoozelooTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SnoozelooTheme {
-        Greeting("Android")
     }
 }
