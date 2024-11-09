117f8d6117ccaf38e396ae3c07de365e5b83b7b9
Tonnie <vontonnie@gmail.com>
2024-11-09 16:54:31 +0300

    Add AlarmDao class

--- app/src/main/java/com/tonyxlab/data/database/dao/AlarmDao.kt
+++ app/src/main/java/com/tonyxlab/data/database/dao/AlarmDao.kt
@@ -1,11 +1,23 @@
 package com.tonyxlab.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.tonyxlab.data.database.entity.AlarmEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface AlarmDao : BaseDao<AlarmEntity>{
+interface AlarmDao : BaseDao<AlarmEntity> {
 
+    @Query("SELECT * FROM alarms_table ORDER BY duration_to_next_trigger ASC")
+    fun getAlarms(): Flow<List<AlarmEntity>>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun createAlarm(entity: AlarmEntity): Long
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateAlarm(entity: AlarmEntity)
+    
 }
