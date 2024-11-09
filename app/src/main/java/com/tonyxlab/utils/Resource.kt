924150daf6a3b4c2e8261b8c11204f366f5ec181
Tonnie <vontonnie@gmail.com>
2024-11-09 15:07:51 +0300

    Add Resource Util class

---
+++ app/src/main/java/com/tonyxlab/utils/Resource.kt
@@ -0,0 +1,18 @@
+package com.tonyxlab.utils
+
+
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data]"
+            is Error -> "Error[message=$message, data=$data]"
+        }
+    }
+}
