8852d00d013b5e338021fc82058a36c83ca2ec9f
Tonnie <vontonnie@gmail.com>
2024-11-11 07:12:46 +0300

    Update Resource.kt

--- app/src/main/java/com/tonyxlab/utils/Resource.kt
+++ app/src/main/java/com/tonyxlab/utils/Resource.kt
@@ -1,18 +1,15 @@
 package com.tonyxlab.utils
 
 
+sealed class Resource<out R> {
 
-sealed class Resource<T>(
-    val data: T? = null,
-    val message: String? = null
-) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    data class Success<out T>(val data: T) : Resource<T>()
+    data class Error(val exception: Exception) : Resource<Nothing>()
 
     override fun toString(): String {
         return when (this) {
             is Success<*> -> "Success[data=$data]"
-            is Error -> "Error[message=$message, data=$data]"
+            is Error -> "Error[exception=$exception]"
         }
     }
 }
