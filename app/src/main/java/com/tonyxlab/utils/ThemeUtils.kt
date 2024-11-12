a0b976b6ecf9e1457d9beeca9a224aa5777ce273
Tonnie <vontonnie@gmail.com>
2024-11-12 19:29:39 +0300

    Add Royal Blue color

---
+++ app/src/main/java/com/tonyxlab/utils/ThemeUtils.kt
@@ -0,0 +1,8 @@
+package com.tonyxlab.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import com.tonyxlab.R
+
+@Composable
+fun getRoyalBlueComposeColor() = colorResource(R.color.blue_600)
