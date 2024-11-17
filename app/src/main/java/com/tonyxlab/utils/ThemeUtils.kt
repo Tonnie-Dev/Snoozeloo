e40f5f75f36ba004b3a359aa02bc724a0e070409
Tonnie <vontonnie@gmail.com>
2024-11-17 08:03:29 +0300

    Update Theme Utils

--- app/src/main/java/com/tonyxlab/utils/ThemeUtils.kt
+++ app/src/main/java/com/tonyxlab/utils/ThemeUtils.kt
@@ -5,4 +5,7 @@ import androidx.compose.ui.res.colorResource
 import com.tonyxlab.R
 
 @Composable
-fun getRoyalBlueComposeColor() = colorResource(R.color.blue_600)
+fun getBlue_600() = colorResource(R.color.blue_600)
+
+@Composable
+fun getBlue_100() = colorResource(R.color.blue_100)
