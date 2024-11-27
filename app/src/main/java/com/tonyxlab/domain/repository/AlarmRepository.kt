ca48260ee3ef7cff3adf0e8ae5f5a6d8e18fd9d8
Tonnie <vontonnie@gmail.com>
2024-11-27 07:40:01 +0300

    Update Alarm Repository class

--- app/src/main/java/com/tonyxlab/domain/repository/AlarmRepository.kt
+++ app/src/main/java/com/tonyxlab/domain/repository/AlarmRepository.kt
@@ -1,15 +1,21 @@
 package com.tonyxlab.domain.repository
 
 
+import android.net.Uri
 import com.tonyxlab.domain.model.AlarmItem
 import com.tonyxlab.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface AlarmRepository {
 
+    val ringtoneFlow: Flow<Uri?>
+
     fun getAlarms(): Flow<List<AlarmItem>>
     suspend fun getAlarmById(alarmId:String): Resource<AlarmItem>
     suspend fun createAlarm(alarmItem: AlarmItem): Resource<Boolean>
     suspend fun updateAlarm(alarmItem: AlarmItem): Resource<Boolean>
     suspend fun deleteAlarm(alarmItem: AlarmItem): Resource<Boolean>
+    suspend fun saveRingtone(uri: Uri)
+
+
 }
