bb9aecba7020ae5644940adb1aa9c33c48bd0d79
Tonnie <vontonnie@gmail.com>
2024-11-27 08:08:11 +0300

    Add PickRingtoneUseCase

---
+++ app/src/main/java/com/tonyxlab/domain/usecases/PickRingtoneUseCase.kt
@@ -0,0 +1,20 @@
+package com.tonyxlab.domain.usecases
+
+import android.net.Uri
+import com.tonyxlab.domain.repository.AlarmRepository
+import com.tonyxlab.domain.ringtone.RingtonePicker
+import kotlinx.coroutines.flow.Flow
+
+class PickRingtoneUseCase(
+    private val picker: RingtonePicker,
+    private val repository: AlarmRepository
+) {
+    val ringtoneFlow: Flow<Uri?> = repository.ringtoneFlow
+
+    suspend fun selectAndSaveRingtone() {
+        val selectedRingtone = picker.pickRingtone()
+        if (selectedRingtone != null) {
+            repository.saveRingtone(selectedRingtone)
+        }
+    }
+}
