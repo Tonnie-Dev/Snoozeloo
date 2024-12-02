b44b7130ef4f0320c0bcb5f6ff81f1b99f71d670
Tonnie <vontonnie@gmail.com>
2024-12-02 19:14:45 +0300

    Update RepositoryModule.kt

--- app/src/main/java/com/tonyxlab/di/RepositoryModule.kt
+++ app/src/main/java/com/tonyxlab/di/RepositoryModule.kt
@@ -4,11 +4,13 @@ import androidx.activity.ComponentActivity
 import com.tonyxlab.data.database.converters.JsonSerializerImpl
 import com.tonyxlab.data.receiver.AlarmHandlerImpl
 import com.tonyxlab.data.repository.AlarmRepositoryImpl
+import com.tonyxlab.data.repository.RingtoneFetcherImpl
 import com.tonyxlab.data.ringtoneimpl.RingtonePickerImpl
 import com.tonyxlab.data.schedulerimpl.AlarmSchedulerImpl
 import com.tonyxlab.domain.json.JsonSerializer
 import com.tonyxlab.domain.model.DayChipState
 import com.tonyxlab.domain.repository.AlarmRepository
+import com.tonyxlab.domain.ringtone.RingtoneFetcher
 import com.tonyxlab.domain.ringtone.RingtonePicker
 import com.tonyxlab.domain.scheduler.AlarmHandler
 import com.tonyxlab.domain.scheduler.AlarmScheduler
@@ -48,6 +50,9 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindComponentActivity(activity: ComponentActivity): ComponentActivity
+
+    @Binds
+    abstract fun bindRingtoneFetcher(ringtoneFetcherImpl: RingtoneFetcherImpl): RingtoneFetcher
 
 
 
