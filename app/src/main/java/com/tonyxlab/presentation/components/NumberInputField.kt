28c0b584878250ef119d69733dd687706f5be720
Tonnie <vontonnie@gmail.com>
2024-12-07 16:23:16 +0300

    Update NumberInputField.kt

--- app/src/main/java/com/tonyxlab/presentation/components/NumberInputField.kt
+++ app/src/main/java/com/tonyxlab/presentation/components/NumberInputField.kt
@@ -1,29 +1,21 @@
 package com.tonyxlab.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
@@ -32,20 +24,17 @@ import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.tonyxlab.presentation.ui.theme.LocalSpacing
 import com.tonyxlab.presentation.ui.theme.SnoozelooTheme
-import com.tonyxlab.utils.getHourString
-import com.tonyxlab.utils.now
-import kotlinx.datetime.LocalDateTime
 
 
 @Composable
 fun NumberInputField(
-    initialText: String,
-    range: IntRange,
-    allowedCharacters: Int = 2,
+    value: String,
+    isError: Boolean,
+    onValueChanged: ((String) -> Unit)?,
     modifier: Modifier = Modifier
 ) {
-    var text by remember { mutableStateOf(initialText) }
-    var isError by remember { mutableStateOf(false) }
+
+
     val spacing = LocalSpacing.current
     Box(
             modifier = modifier
@@ -61,16 +50,8 @@ fun NumberInputField(
 
         BasicTextField(
 
-                value = text,
-                onValueChange = { newInput ->
-
-                    if (newInput.length <= allowedCharacters) {
-
-                        text = newInput
-                        isError = newInput.toIntOrNull()
-                                ?.let { int -> int !in range } ?: true
-                    }
-                },
+                value = value,
+                onValueChange = { onValueChanged?.invoke(it) },
                 visualTransformation = { annotatedString ->
                     if (isError) {
                         TransformedText(
@@ -104,16 +85,24 @@ fun NumberInputField(
 private fun NumberInputFieldPreview() {
     val spacing = LocalSpacing.current
     SnoozelooTheme {
-
         Surface {
 
+            Column {
+                NumberInputField(
+                        value = "13",
+                        onValueChanged = {},
+                        isError = false,
 
-            NumberInputField(
+                        )
 
-                    initialText = LocalDateTime.now()
-                            .getHourString(),
-                    range = 0..23
-            )
+                Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                NumberInputField(
+                        value = "13",
+                        onValueChanged = {},
+                        isError = true,
+
+                        )
+            }
         }
     }
 }
