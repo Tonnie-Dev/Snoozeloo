5320ea377b63b4415b63fb37f551b7a363fc6267
Tonnie <vontonnie@gmail.com>
2024-12-24 08:00:33 +0300

    Create GetFutureDateUseCase.kt

---
+++ app/src/main/java/com/tonyxlab/domain/usecases/GetFutureDateUseCase.kt
@@ -0,0 +1,63 @@
+package com.tonyxlab.domain.usecases
+
+import com.tonyxlab.domain.model.DayChipState
+import com.tonyxlab.utils.now
+import com.tonyxlab.utils.plusDays
+import kotlinx.datetime.LocalDateTime
+import javax.inject.Inject
+
+class GetFutureDateUseCase @Inject constructor() {
+
+    operator fun invoke(alarmTriggerTime: LocalDateTime, list: List<DayChipState>): LocalDateTime {
+        var finalDateTime: LocalDateTime? = null
+        val currentTime = LocalDateTime.now()
+        val dayOfWeek = currentTime.dayOfWeek.value
+
+        val checkedDays = mutableListOf<Int>()
+        for (i in list.indices) {
+
+
+            if (list[i].isEnabled) {
+
+                checkedDays.add(i)
+            }
+        }
+
+        if (checkedDays.contains(dayOfWeek)) {
+            finalDateTime = carryTime(alarmTriggerTime = alarmTriggerTime)
+
+
+        } else if (checkedDays.isEmpty()) {
+
+            finalDateTime = carryTime(alarmTriggerTime = alarmTriggerTime)
+        } else {
+
+            val firstDay = checkedDays.first()
+
+            if (firstDay < dayOfWeek) {
+
+                finalDateTime = alarmTriggerTime.plusDays(dayOfWeek - firstDay)
+            } else {
+
+                finalDateTime = alarmTriggerTime.plusDays(firstDay - dayOfWeek)
+            }
+
+        }
+
+        return finalDateTime ?: currentTime.plusDays(1)
+    }
+
+    private fun carryTime(alarmTriggerTime: LocalDateTime): LocalDateTime {
+
+        val currentTime = LocalDateTime.now()
+        return if (alarmTriggerTime < currentTime) {
+            //earlier
+            alarmTriggerTime
+        } else {
+
+            //after
+            alarmTriggerTime.plusDays(1)
+        }
+    }
+
+}
