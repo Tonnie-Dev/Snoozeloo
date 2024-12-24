d938c6224435f96defd2536124c57df88aca7dec
Tonnie <vontonnie@gmail.com>
2024-12-24 08:00:42 +0300

    Update SerializerModule.kt

--- app/src/main/java/com/tonyxlab/di/SerializerModule.kt
+++ app/src/main/java/com/tonyxlab/di/SerializerModule.kt
@@ -1,13 +1,11 @@
 package com.tonyxlab.di
 
 import com.tonyxlab.domain.model.DayChipState
-import com.tonyxlab.domain.model.Ringtone
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.builtins.ListSerializer
 import javax.inject.Singleton
 
 @Module
@@ -21,12 +19,12 @@ object SerializerModule {
     }
 
 
-   /* @Provides
-    @Singleton
-    fun provideDayChipStateListSerializer(): KSerializer<List<DayChipState>> {
+    /* @Provides
+     @Singleton
+     fun provideDayChipStateListSerializer(): KSerializer<List<DayChipState>> {
 
-        return ListSerializer(DayChipState.serializer())
-    }*/
+         return ListSerializer(DayChipState.serializer())
+     }*/
 
 
     /*@Provides
